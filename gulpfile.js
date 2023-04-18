7e115319bd8610b34bfebc4812453d59cfca8e45
Yab1 <yabilisanu@gmail.com>
2023-04-18 10:24:30 +0300

    source changed

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-const { src, dest, watch, series } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src('src/**/*.scss').pipe(sass()).pipe(dest('public'));
+  return src("src/**/*.scss").pipe(sass()).pipe(dest("src"));
 }
 
 function watchTask() {
-  watch(['src/**/*.scss'], buildStyles);
+  watch(["src/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
