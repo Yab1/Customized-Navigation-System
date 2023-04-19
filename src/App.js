570de44fad24796ff07fd37479842fb424a304cb
Yab1 <yabilisanu@gmail.com>
2023-04-19 03:04:51 +0300

    Render Body component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import HeaderAppBar from "./components/Header/AppBar";
 import Form from "./pages/Forms/Forms";
+import Body from "./components/Body/Body";
 
 function App() {
   return (
     <div className="App">
       {/* <Form /> */}
       <HeaderAppBar />
+      <Body />
     </div>
   );
 }
