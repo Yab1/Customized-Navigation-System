02b5ed60a45da0cad54ad88f5773b59195a4c8b9
Yab1 <yabilisanu@gmail.com>
2023-04-20 14:18:25 +0300

    conditional render

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HeaderAppBar from "./components/Header/AppBar";
 import Form from "./pages/Forms/Forms";
 import Body from "./components/Body/Body";
+import ProductContextProvider from "./context/ProductContext";
 
 function App() {
+  const [state, setState] = useState({ isLogging: false, logged: false });
+  const [isLogging, setIsLogging] = useState(false);
+  const [logged, setLogged] = useState(false);
+
+  const isLoggingSetter = () => {
+    setIsLogging(!isLogging);
+  };
+  const loggedSetter = () => {
+    setLogged(!logged);
+  };
+  console.log(isLogging);
+
   return (
-    <div className="App">
-      {/* <Form /> */}
-      <HeaderAppBar />
-      <Body />
-    </div>
+    <ProductContextProvider>
+      <Router>
+        <div className="App">
+          <HeaderAppBar />
+          <Body />
+          <Routes>
+            <Route path="/form" element={<Form />} />
+          </Routes>
+        </div>
+      </Router>
+    </ProductContextProvider>
   );
 }
 export default App;
