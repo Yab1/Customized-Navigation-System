c7a86f10aa3bb6c11dc4cec6b1be4a5302b3579d
Yab1 <yabilisanu@gmail.com>
2023-04-20 14:16:17 +0300

    removed the states and replaced by a context

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,25 +1,20 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
+import { ProductContext } from "../../context/ProductContext";
 import Product from "./product";
-import useFetch from "../../hooks/useFetch";
 
 function Body() {
-  const [products, setProducts] = useState([]);
-  const productUrl = "https://fakestoreapi.com/products";
-  const { data, setUrl } = useFetch(productUrl);
+  const { productInfo } = useContext(ProductContext);
 
-  useEffect(() => {
-    setProducts(data);
-  }, [data]);
   return (
     <Container disableGutters maxWidth="lg">
       <Grid container spacing={3}>
-        {products.map((product) => (
+        {/* {products.map((product) => (
           <Grid item xs={12} sm={6} lg={4} xl={3}>
             <Product key={product.key} product={product} />
           </Grid>
-        ))}
+        ))} */}
       </Grid>
     </Container>
   );
